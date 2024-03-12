592997d37de93a54444dc6829277856d9973541a
carbancle <carbancle2@gmail.com>
2024-03-13 04:57:12 +0900

    edit backend data

--- backend/api/index.ts
+++ backend/api/index.ts
@@ -13,16 +13,16 @@ const app: Application = express();
 connectToDB();
 
 // app.use("가상경로", "실제경로") 형식으로도 사용 가능
-app.use(express.static(path.join("../src/uploads")));
+app.use(express.static(path.join("/uploads")));
 // cross-origin resource sharing:
 //프론트서버와 백엔드서버의 port가 다른 경우, cors를 활성화 하여 데이터 공유를 할 수 있도록 함
 app.use(cors());
 // json 요청을 읽을 수 없기 때문에, json을 받을 수 있도록 설정
 app.use(express.json());
 
-app.use("/users", require("../src/routes/users"));
-app.use("/products", require("../src/routes/products"));
-app.use("/route-test", require("../src/routes/route-test"));
+app.use("/users", require("./routes/users"));
+app.use("/products", require("./routes/products"));
+app.use("/route-test", require("./routes/route-test"));
 
 // error 처리, error 타입을 임시값으로 any로 설정, 추후 수정
 app.use((error: any, req: Request, res: Response, next: NextFunction) => {
